1bbb0edb27e193c664d4901bee0464caf947f2d9
Anderson Bornachera <anderson@example.com>
2025-09-05 16:10:56 -0500

    Fix Whitelist

--- app.py
+++ app.py
@@ -48,9 +48,8 @@ scheduler.start()
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-WHITELIST = {
-    "573028432451"  # Anderson
-}
+WHITELIST = {num.strip() for num in os.getenv("WHITELIST", "").split(",") if num.strip()}
+
 FALLBACK_MESSAGE = "Lo siento, cambié de número, escríbeme al wa.me/573028432451 Gracias."
 
 def send_whatsapp_message(to, text):
